cc6c0aa3641d6b55ef1e0e0cc70549c43a8c4be3
Marcin Naglik <marcin.naglik@gmail.com>
2013-05-12 17:16:02 +0200

    koniec lekcji. ostatni wpis

--- README.md
+++ README.md
@@ -3,3 +3,14 @@ To sa pierwsze lekcje rubiego.
 Dzis bedziemy instalowas ruby
 
 http://www.ruby-doc.org/core-1.9.3/String.html
+
+
+# Class
+(dokumentacje mozna znalezc pod adresem http://ruby-doc.org/core-2.0/)
+
+String
+Symbol
+Fixnum / Float / Decimal
+
+Array
+Hash
--- hello_world.rb
+++ hello_world.rb
@@ -13,3 +13,35 @@ if a > 1
 else
   puts  "a <= 1"
 end
+
+
+def napisz_cos(cos)
+  puts "#{cos}"
+end
+
+napisz_cos("nie wiem co")
+
+
+def napisz_cos(parametry = {})
+  puts "#{parametry[:cos]}"
+end
+
+
+class NaszaKlasa
+  def initialize
+    puts "konstruktor"
+  end
+
+  def zrob_cos
+    puts "zrobilem cos"
+  end
+
+  def self.zrob_cos
+    puts "zrobilem cos statycznie"
+  end
+end
+
+# a = NaszaKlasa.new
+# a.zrob_cos
+
+NaszaKlasa.zrob_cos
